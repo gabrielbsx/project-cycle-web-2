15cbadd5b823f0635c18903cc7d03e9703fd027a
Gabriel <gabrielturing@gmail.com>
2023-06-24 02:19:08 -0300

    feat: ensure that abstract crud is callable

--- packages/cycle-two/src/routes.ts
+++ packages/cycle-two/src/routes.ts
@@ -2,8 +2,15 @@ import { Express } from "express";
 import { createUserController } from "./modules/create-user/create-user.controller";
 import { signInController } from "./modules/sign-in/signin.controller";
 import { guestUser } from "./middlewares/guest-user";
+import { authUser } from "./middlewares/auth-user";
+import { abstractCRUDController } from "./modules/abstract-crud/abstract-crud.controller";
 
 export const routesFactory = (app: Express) => {
   app.post("/users", guestUser, createUserController);
   app.post("/auth", guestUser, signInController);
+  app.post("/abstract-crud/:entity", authUser, abstractCRUDController);
+  app.get("/abstract-crud/:entity", authUser, abstractCRUDController);
+  app.get("/abstract-crud/:entity/:id", authUser, abstractCRUDController);
+  app.delete("/abstract-crud/:entity", authUser, abstractCRUDController);
+  app.put("/abstract-crud/:entity", authUser, abstractCRUDController);
 };
