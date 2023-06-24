3066c543b34878e83bbb7ca209228792e5aaf825
Gabriel <gabrielturing@gmail.com>
2023-06-24 02:19:26 -0300

    feat: add ok helper

--- packages/cycle-two/src/utils/http.ts
+++ packages/cycle-two/src/utils/http.ts
@@ -2,22 +2,41 @@ export const unauthorized = (message) => {
   const error = new Error(message);
   error.name = "UnauthorizedError";
   return error;
-}
+};
 
 export const notFound = (message) => {
   const error = new Error(message);
   error.name = "NotFoundError";
   return error;
-}
+};
 
 export const validationError = (message) => {
   const error = new Error(message);
   error.name = "ValidationError";
   return error;
-}
+};
 
 export const badRequest = (message) => {
   const error = new Error(message);
   error.name = "BadRequestError";
   return error;
-}
+};
+
+export const ok = ({
+  message,
+  data,
+  statusCode,
+  pagination,
+}: {
+  message?: string;
+  data: any;
+  statusCode: number;
+  pagination?: any;
+}) => {
+  return {
+    statusCode,
+    message,
+    pagination,
+    data,
+  };
+};
