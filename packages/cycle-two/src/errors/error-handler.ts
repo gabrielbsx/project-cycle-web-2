5422d84d69d1ea50af8e91b3bd2d5ebe2abaf446
Gabriel <gabrielturing@gmail.com>
2023-06-23 23:16:33 -0300

    feat: define name of errors and treatments

--- packages/cycle-two/src/errors/error-handler.ts
+++ packages/cycle-two/src/errors/error-handler.ts
@@ -6,6 +6,16 @@ export const errorHandler = (
   response: Response,
   _next: () => void
 ) => {
-  console.error(error);
-  return response.status(500).json({ message: error.message });
+  switch (error.name) {
+    case "ValidationError":
+      return response.status(400).json({ message: error.message });
+    case "UnauthorizedError":
+      return response.status(401).json({ message: error.message });
+    case "NotFoundError":
+      return response.status(404).json({ message: error.message });
+    case "BadRequestError":
+      return response.status(400).json({ message: error.message });
+    default:
+      return response.status(500).json({ message: error.message });
+  }
 };
